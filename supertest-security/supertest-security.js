fa262923d78e38de18c26bc37711af44c9083070
nukuutos <nukuutos@gmail.com>
2022-10-02 21:17:17 +1000

    Delete unused functionality in `superset-security.js`

--- supertest-security/supertest-security.js
+++ supertest-security/supertest-security.js
@@ -3,58 +3,37 @@ const app = require("../app");
 
 class SupertestSecurity {
   constructor(app, { template, routeParams, method = "post" }) {
-    this.agent = supertest.agent(app);
+    this.app = app;
     this.template = template;
     this.routeParams = routeParams;
     this.method = method;
   }
 
-  getUrl(customRouteParams) {
+  getUrl() {
     const { routeParams, template } = this;
-
-    const urlRouteParams = { ...routeParams, ...customRouteParams };
 
     const templateArray = template.split("/");
 
-    for (const param in urlRouteParams) {
+    for (const param in routeParams) {
       const paramIndex = templateArray.indexOf(`:${param}`);
       if (paramIndex === -1) throw new Error(`:${param} - no route param in template!`);
-      templateArray[paramIndex] = urlRouteParams[param];
+      templateArray[paramIndex] = routeParams[param];
     }
 
     return encodeURI(templateArray.join("/"));
   }
 
-  // setHeaders(request) {
-  //   const headers = Object.keys(this.headers);
-
-  //   for (const header of headers) {
-  //     request.set(header, this.headers[header]);
-  //   }
-
-  //   return request;
-  // }
-
-  request(customRouteParams = {}) {
-    const { agent, method } = this;
-
-    const url = this.getUrl(customRouteParams);
-
-    console.log(method, url);
-
-    return agent[method](url);
-  }
-
   async testBodyFields(tests, cb) {
     const promises = [];
 
+    const { method } = this;
+    const url = this.getUrl();
+
     for (const test of tests) {
-      const promise = this.request().send(test.data);
-      // const promise = supertest(app).put("/user/123").send(test.data);
+      const promise = supertest(app)[method](url).send(test.data);
       promises.push(promise);
     }
 
-    // let results = await Promise.all(promises.map((p) => p.catch((e) => console.log(e))));
     let results = await Promise.all(promises);
 
     results = results.map(({ value }) => value);
@@ -67,8 +46,11 @@ class SupertestSecurity {
   async testQueryParams(tests, cb) {
     const promises = [];
 
+    const { method } = this;
+    const url = this.getUrl();
+
     for (const test of tests) {
-      const promise = this.request().query(test.data);
+      const promise = supertest(app)[method](url).query(test.data);
       promises.push(promise);
     }
 
@@ -83,8 +65,11 @@ class SupertestSecurity {
   async testRouteParams(tests) {
     const promises = [];
 
+    const { method } = this;
+
     for (const test of tests) {
-      const promise = this.request(test.data);
+      const url = this.getUrl(test.data);
+      const promise = supertest(app)[method](url)(test.data);
       promises.push(promise);
     }
 
