696ca2d21b7bcdfcdfc38b6a6fcc175faa54bf89
nukuutos <nukuutos@gmail.com>
2022-11-10 14:26:06 +1000

    Add new payloads

--- src/attack-types.js
+++ src/attack-types.js
@@ -1,3 +1,7 @@
-exports.XSS = "xss";
-exports.SQL_INJECTION = "sql-injection";
-exports.NOSQL_INJECTION = "nosql-injection";
+exports.XSS = 'xss';
+exports.SQL_INJECTION = 'sql-injection';
+exports.NOSQL_INJECTION = 'nosql-injection';
+exports.GRAPHQL_INJECTION = 'graphql-injection';
+exports.PATH_TRAVERSAL = 'path-traversal';
+exports.UNIX_COMMAND_INJECTION = 'unix-command-injection';
+exports.WINDOWS_COMMAND_INJECTION = 'windows-command-injection';
